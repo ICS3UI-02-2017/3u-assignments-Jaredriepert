9abb5745b43aae635d7dc0ab8f3fd6e415334dea
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-02-28 11:21:16 -0500

    Initilize commit

--- src/Examples/Input_OutPut.java
+++ src/Examples/Input_OutPut.java
@@ -4,12 +4,13 @@
  */
 package Examples;
 
-import java.util.Scanner;
+
 
 /**
  *
  * @author riepj9547
  */
+import java.util.Scanner;
 public class Input_OutPut {
 
     /**
@@ -17,21 +18,36 @@ public class Input_OutPut {
      */
     public static void main(String[] args) {
         System.out.print("Hello user, please enter your name:");
+        
         Scanner input = new Scanner(System.in);
+        
         String name = input.nextLine();
+        
         System.out.println("Welcome "+ name +", how old are you?");
+        
         int age = input.nextInt();
+        
         System.out.println("Okay "+ name +" you are "+ age + " years old.");
+        
         int yearborn = 2018 - age;
+        
         System.out.println("Does this mean you were born in " + yearborn + "?");
+        
         input.nextLine(); //clear out all enter spaces
         String yesNo = input.nextLine().toLowerCase();
+        
         if(yesNo.equals("yes")){
+            
              System.out.println("Okay sweet so you were born in "+ yearborn+ " you are "+age+" years old, and your name is "+name);
         }else{
+            
             System.out.println("Oh okay what is it then?");
-            yearborn =input.nextInt();
+            
+           yearborn =input.nextInt();
+            
             System.out.println("Got it, you were born in "+ yearborn+ " you are "+age+" years old, and your name is "+name);
+            
         }
+     
     }
 }
