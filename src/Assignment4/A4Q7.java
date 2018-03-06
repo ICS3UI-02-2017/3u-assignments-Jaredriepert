16776693719551466ae809ec2685a457fcb79dc4
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-03-06 11:17:06 -0500

    Initilize commit

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -27,13 +27,17 @@ public class A4Q7 {
         int turn = 1;
         int p1Spot = 1;
         int p2Spot = 1;
-        while (true) {
+        int winner = 0;
+        
+        while (winner==0) {
             //when it is players one turn let him/her role
             while (turn == 1) {
                 System.out.print(p1 + " enter dice count: ");
                 int diceCount = input.nextInt();
                 if (diceCount == 0) {
                     //terminate
+                    System.out.println("You Quit!");
+                    System.exit(0);
                 }
                 if (diceCount == 1 || diceCount > 12) {
                     System.out.print("Invalid number, please try again:");
@@ -44,28 +48,38 @@ public class A4Q7 {
                     //if you land on a latter go up
                     if (p1Spot == 9) {
                         p1Spot = 34;
+                        System.out.println("Yay! A ladder!");
                     } else if (p1Spot == 40) {
                         p1Spot = 64;
+                        System.out.println("Yay! A ladder!");
                     } else if (p1Spot == 67) {
                         p1Spot = 86;
-                    } else if (p1Spot == 67) {
-                        p1Spot = 86;
+                        System.out.println("Yay! A ladder!");
                         //if you land on a snake go down
                     } else if (p1Spot == 99) {
                         p1Spot = 77;
+                        System.out.println("Oh no! A snake!");
                     } else if (p1Spot == 90) {
                         p1Spot = 48;
+                        System.out.println("Oh no! A snake!");
                     } else if (p1Spot == 54) {
                         p1Spot = 19;
+                        System.out.println("Oh no! A snake!");
+                    } else if (p1Spot > 100) {
+                        p1Spot = p1Spot - diceCount;
+                        System.out.println("Oops looks like you can't move");
+                        //if you are on spot 100 you win
                     }
-                    //if you are on spot 100 you win
                     if (p1Spot == 100) {
                         System.out.println(p1 + " Wins! ");
                         System.out.println(p2 + " lost on space " + p2Spot);
+                        winner=1;
                     } else {
-                    System.out.println(p1 + " is now on space " + p1Spot);
-                    System.out.println(p2 + " is on space " + p2Spot);
-                }
+                        System.out.println(p1 + " is now on space " + p1Spot);
+                        System.out.println(p2 + " is on space " + p2Spot);
+                        System.out.println("------------------------------");
+                        System.out.println("");
+                    }
                 }
                 turn = 2;
             }
@@ -74,6 +88,8 @@ public class A4Q7 {
                 int diceCount = input.nextInt();
                 if (diceCount == 0) {
                     //terminate
+                    System.out.println("You Quit!");
+                    System.exit(0);
                 }
                 if (diceCount == 1 || diceCount > 12) {
                     System.out.print("Invalid number, please try again:");
@@ -81,29 +97,41 @@ public class A4Q7 {
                 }
                 if (diceCount >= 2 && diceCount <= 12) {
                     p2Spot = p2Spot + diceCount;
+                    //if you land on a ladder go up
                     if (p2Spot == 9) {
                         p2Spot = 34;
+                        System.out.println("Yay! A ladder!");
                     } else if (p2Spot == 40) {
                         p2Spot = 64;
+                        System.out.println("Yay! A ladder!");
                     } else if (p2Spot == 67) {
                         p2Spot = 86;
-                    } else if (p2Spot == 67) {
-                        p2Spot = 86;
+                        System.out.println("Yay! A ladder!");
                         //if you land on a snake go down
                     } else if (p2Spot == 99) {
                         p2Spot = 77;
+                        System.out.println("Oh no! A snake!");
                     } else if (p2Spot == 90) {
                         p2Spot = 48;
+                        System.out.println("Oh no! A snake!");
                     } else if (p2Spot == 54) {
                         p2Spot = 19;
+                        System.out.println("Oh no! A snake!");
+                    } else if (p2Spot > 100) {
+                        p2Spot = p2Spot - diceCount;
+                        System.out.println("Oops looks like you can't move");
+                        //if they are on spot 100 they win
                     }
-                    //if they are on spot 100 they win
                     if (p2Spot == 100) {
                         System.out.println(p2 + " Wins! ");
                         System.out.println(p1 + " lost on space " + p1Spot);
+                        winner=1;
                     } else {
                         System.out.println(p2 + " is now on space " + p2Spot);
                         System.out.println(p1 + " is on space " + p1Spot);
+                        System.out.println("------------------------------");
+                        System.out.println("");
+                        
                     }
                 }
                 turn = 1;
