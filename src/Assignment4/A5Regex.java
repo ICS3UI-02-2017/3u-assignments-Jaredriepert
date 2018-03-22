eb2cd06de55d244b1fe90726cd4ff604dcb6e170
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-03-22 11:18:12 -0400

    Initilize commit

--- src/Assignment4/A5Regex.java
+++ src/Assignment4/A5Regex.java
@@ -4,7 +4,6 @@
  */
 package Assignment4;
 
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,26 +19,44 @@ public class A5Regex {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        while(true){        
+        while (true) {
             System.out.println();
             System.out.println("----------------------------------");
-        System.out.println("Please enter text to be translated:");
-        //what is the text you want to translate?
-        CharSequence text = input.nextLine();        
-        //the regex pattern
-        Pattern p = Pattern.compile("(?i)[aeiou]+");
-        Matcher m = p.matcher(text);
-        //some crazy stuff
-        StringBuffer bufStr = new StringBuffer();
-        boolean flag = false;
-        //while it finds the regex pattern replace it with "ub" followed up by the pattern it just found
-        while (flag = m.find()){
-            String rep = m.group();
-            m.appendReplacement(bufStr, "ub"+m.group());
+            System.out.println("Please enter text to be translated:");
+            //what is the text you want to translate?
+            CharSequence text = input.nextLine();
+            //the regex pattern
+            Pattern p = Pattern.compile("(?i)[aeiou]+");
+            Matcher m = p.matcher(text);
+            //some crazy stuff
+            StringBuffer bufStr = new StringBuffer();
+            boolean flag = false;
+            //while it finds the regex pattern replace it with "ub" followed up by the pattern it just found
+            while (flag = m.find()) {
+                String rep = m.group();
+                m.appendReplacement(bufStr, "ub" + m.group());
+            }
+            m.appendTail(bufStr);
+            String result = bufStr.toString();
+            System.out.println(result);
+            System.out.println("Would you like to translate it back to English?  Yes or No?");
+            String answer = input.nextLine();
+            answer.toLowerCase();
+            if (answer.contentEquals("yes")) {
+                text = result;
+                p = Pattern.compile("(?i)ub([aieou])");
+                m = p.matcher(text);
+                bufStr = new StringBuffer();
+                flag = false;
+                while (flag = m.find()) {
+                String rep = m.group();
+                m.appendReplacement(bufStr, "" + m.group(1));
+            }
+            m.appendTail(bufStr);
+            result = bufStr.toString();
+            System.out.println(result);
+            }
         }
-        m.appendTail(bufStr);
-        String result = bufStr.toString();
-        System.out.println(result);
-        }
+
     }
 }
--- src/Assignment4/A5String.java
+++ src/Assignment4/A5String.java
@@ -18,31 +18,60 @@ public class A5String {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a text...");
-        //take the text to be translated
-        String origword = input.nextLine();
-        origword = origword.toLowerCase();
-        int length = origword.length();
-        //the string to check for what is in the tex above
-        String vowels = "aeiou";
-        //The new string everything will be printed to
-        String newWord = "";
-        for (int i = 0; i < length; i++) {
-            //if the original text contains any of the vowels and there is not a vowel before it
-            //even if it is the first letter of the word, print everything up to the vowel into the new text and add "ub"
-            if (vowels.indexOf(origword.charAt(i)) != -1) {
-                if (i!=0&&vowels.indexOf(origword.charAt(i-1)) == -1) {
-                    newWord = newWord + "ub" + origword.charAt(i);
-                }else if(i!=0){
-                    newWord=newWord+origword.charAt(i);
-                }else if(i==0){
-                    newWord = newWord + "ub" + origword.charAt(i);
+        while (true) {
+            System.out.println("Enter a text...");
+            //take the text to be translated
+            String origword = input.nextLine();
+            origword = origword.toLowerCase();
+            int length = origword.length();
+            //the string to check for what is in the tex above
+            String pattern = "aeiou";
+            //The new string everything will be printed to
+            String newWord = "";
+            for (int i = 0; i < length; i++) {
+                //if the original text contains any of the vowels and there is not a vowel before it
+                //even if it is the first letter of the word, print everything up to the vowel into the new text and add "ub"
+                if (pattern.indexOf(origword.charAt(i)) != -1) {
+                    if (i != 0 && pattern.indexOf(origword.charAt(i - 1)) == -1) {
+                        newWord = newWord + "ub" + origword.charAt(i);
+                    } else if (i != 0) {
+                        newWord = newWord + origword.charAt(i);
+                    } else if (i == 0) {
+                        newWord = newWord + "ub" + origword.charAt(i);
+                    }
+                    //If it is not a vowel just print it to the new text
+                } else {
+                    newWord = newWord + origword.charAt(i);
                 }
-            //If it is not a vowel just print it to the new text
-            } else {
-                newWord = newWord + origword.charAt(i);
+            }
+            System.out.println(newWord);
+            System.out.println("Would you like to translate this back into English?  Yes or No?");
+            String answer = input.nextLine();
+            answer.toLowerCase();
+            if (answer.contentEquals("yes")) {
+                pattern = "u";
+                String pattern2 = "b";
+                origword = newWord;
+                length = origword.length();
+                newWord = "";
+                for (int i = 0; i < length; i++) {
+                    //if the original text contains any of the vowels and there is not a vowel before it
+                    //even if it is the first letter of the word, print everything up to the vowel into the new text and add "ub"
+                    if (pattern.indexOf(origword.charAt(i)) != -1) {
+                        if ((pattern.indexOf(origword.charAt(i)) != -1) && pattern2.indexOf(origword.charAt(i + 1)) != -1) {
+                            i = i + 2;
+                            newWord = newWord + origword.charAt(i);
+                        } else {
+                            newWord = newWord + origword.charAt(i);
+                        }
+
+                    }else{
+                        newWord = newWord + origword.charAt(i);
+                    }
+                }
+                System.out.println(newWord);
             }
         }
-        System.out.println(newWord);
+
     }
 }
