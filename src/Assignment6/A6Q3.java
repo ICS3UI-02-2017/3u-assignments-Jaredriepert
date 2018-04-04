bacadf7464deebe10ee002c1fbfbd02fa13ec4df
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-04-04 11:13:17 -0400

    Initilize commit

--- src/Assignment6/A6Q3.java
+++ src/Assignment6/A6Q3.java
@@ -21,16 +21,27 @@ public class A6Q3 {
         int[] numbers = new int[2];
         System.out.println("Please enter the two numbers");
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i]=input.nextInt();
+            numbers[i] = input.nextInt();
         }
         //check if the first one is bigger or smaller than the second one
         //first create a buffer int
         int replace = 0;
-        if (numbers[0]>numbers[1]){
-            replace=numbers[1];
-            numbers[1]=numbers[0];
-            numbers[0]=replace;
-            System.out.println("The numbers in ascending order is "+numbers[0]+", "+numbers[1]);
+        //go through the array
+        for (int i = 0; i < numbers.length; i++) {
+            //as long as you are not at the end of the array check to see if the spot is bigger or smaller than the one after it
+            if (i < numbers.length - 1) {
+                //if it is bigger switch places
+                if (numbers[i] > numbers[i + 1]) {
+                    replace = numbers[i + 1];
+                    numbers[i + 1] = numbers[i];
+                    numbers[i] = replace;
+                    System.out.println("The numbers in ascending order is " + numbers[0]+", "+numbers[1]);
+                    //if it is smaller dont switch places
+                } else {
+                    System.out.println("The numbers in ascending order is " + numbers[0]+", "+numbers[1] );
+                    
+                }
+            }
         }
     }
 }
---
+++ src/Assignment6/A6Q4.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author riepj9547
+ */
+public class A6Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create array
+        Scanner input = new Scanner(System.in);
+        int[] marks = new int[10];
+        //fill the array with the marks
+        System.out.println("Please enter 10 marks");
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = input.nextInt();
+        }
+        //begin sorting
+        int x = 0;
+        int y = 1;
+        int replace = 0;
+        //until I have gone through the whole array search trhough it commparing one spot with every other spot
+        while (x < marks.length - 1) {
+            for (y = x + 1; y < marks.length; y++) {
+                if (marks[y] < marks[x]) {
+                    replace = marks[x];
+                    marks[x] = marks[y];
+                    marks[y] = replace;
+                }
+            }
+            x++;
+        }
+        //print out all the numbers in the new order
+        System.out.print("The marks in order are... ");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+    }
+}
